8e98fae92c90335f04139596a057a4fe59ab3279
pl4tin3um <platineumuru@gmail.com>
2025-05-21 21:44:57 -0300

    Add files via upload

---
+++ PruebaVariables.java
@@ -0,0 +1,97 @@
+public class PruebaVariables {
+   public static void main(String[] args) {
+
+// ejercicio 1
+ 
+int edad = 14;
+
+System.out.println("la edad es " + edad);
+// ejercicio 2
+double precio = 45.99;
+// ejercicio 3
+boolean tieneDescuento = true;
+// ejercicio 4
+char inicial = 'K';
+// ejercicio 5
+int puntos = 10;
+puntos = 20;
+System.out.println("los puntos son " + puntos);
+// ejercicio 6
+double NotaFinal = 7;
+System.out.println( NotaFinal >= 6 ? "aprovado" : "suspenso");
+// ejercicio 7
+
+double temperatura;
+temperatura = 36.6;
+System.out.println("la temperatura es " + temperatura);
+// ejercicio 8
+//int 99 = 99;
+// error The left-hand side of an assignment must be a variable
+// syntax error, insert "VariableDeclaratorId" to complete LocalVariableDeclaration
+//syntax error, insert ";" to complete LocalVariableDeclarationStatement
+// ejercicio 9
+int edads = 14;
+boolean esmayor = edads >= 18;
+System.out.println(esmayor ? "es mayor de edad " : "no es mayor de edad");
+// ejercicio 10
+int x = 14, y = 18, z = 20;
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<
+//PARTE DOS
+// ejercicio 1
+//QUE SON LAS VARIABLES EN JAVA
+// una variables es un comando que almacena un valor de un tipo de dato en la memoria
+
+// ejercicio 2
+//los tipos primitivos de variables son
+// int,char,boolean,byte
+//int se encarga de almacenar solo numeros enteros
+//char almacena solo un caracter
+//boolean almacena si es verdadero o falso dependiendo la condicion aplicada
+//byte almacena un numero de 8 bits, pero que solo es entero
+
+/*EJERCICIO 3
+ * CUAL ES LA DIFERENCIA ENTRE DECLARAR UNA VARIABLE Y ASIGNAR UN VALOR
+ * que al declarar una variable solo existe, y al asignarle un valor se le dice que dato es el que debe almacenar
+ */
+
+ /*EJERCICIO 4
+ * QUE SIGIFICIA QUE JAVA SEA UN LENGUAJE FUERTEMENTE TIPADO
+ * seria como que es muy estricto con los tipos de datos* 
+  */
+
+  /*EJERCICIO 5
+   * cual es la syntaxis para declarar una constante, que diferencia tiene con una variable normal
+   * ´que la constante no almacena datos y no tiene un nombre, simplemente hace lo que se le dice
+   * y una variable comun si almacena daotos y tiene un nombre
+   */
+    
+     /*Ejercicio 6
+      * por que  es importante usar nombres descriptivos para las variables
+      * porque es necesario entender el significado de la variable y que sea legible y que no se confunda con otras, ayudando a la progamacion y aque otras personas puedan entender mejor el codigo
+      */
+
+      /*EJERCICIO 7
+       * que es el camel case
+       * no sabemos
+       */
+
+        /*Ejercicio 8
+        *  que pasa si declaras una variable reservada de java
+         * sale un error
+        */
+
+        /*Ejercicio 9
+         * se puede cambiar el tipo de una variable una vez que ha sido declarada
+         * no, cuando mucho lo que se puede hacer es que te de el valor de esa variable como si fuera de otro tipo
+         * pero la variable no cambia, sigue igual
+         */
+
+         /* EJERCICIO 10
+        *  escrible dos ejemplos de nombres de variables validos y dos invalidos explicando por que
+          * nombres validos ejemplo int numerouno, int numerodos deja proque no incumple ningun tipo de "regla"
+          *  nombres invalidos ejemplo int 1numero,no deja porque empieza con un numero
+          *  y el otro ejemplo es int int, no deja porque es una palabra reservada de java   
+          */
+     
+    }
+} trabajo hecho por Kevin Fontes y Franco Reyes
